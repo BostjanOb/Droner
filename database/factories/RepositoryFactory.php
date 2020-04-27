07f68e64e6f22350b7414f20a9923e0a25741eee
Bostjan Oblak <bostjan@muha.cc>
2020-04-28 00:20:41 +0200

    fix repository factory to return unique numbers

--- database/factories/RepositoryFactory.php
+++ database/factories/RepositoryFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Repository::class, function (Faker $faker) {
     return [
-        'id'         => $faker->randomNumber(2),
+        'id'         => $faker->unique()->numberBetween(),
         'name'       => $faker->name,
         'drone_slug' => $faker->slug,
         'git_link'   => $faker->url,
