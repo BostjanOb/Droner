d289f709d240e5cf3bee2b3edd1a38408dca2d30
Bostjan Oblak <bostjan@muha.cc>
2020-06-06 16:46:25 +0200

    fix purging css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    './resources/**/*.php'
-  ],
+  purge: {
+    content: ['./resources/**/*.php'],
+    options: {
+      whitelist: [
+        'w-3',
+        'w-4',
+        'w-5',
+        'w-6',
+
+        'h-3',
+        'h-4',
+        'h-5',
+        'h-6'
+      ]
+    }
+  },
   theme: {
     container: {
       center: true
